79226fdbf210552fb55cd0bff679e6d0a3df3c27
hafizflow <hafizur.rahman.cs@gmail.com>
2024-10-16 12:15:59 +0600

    SocialMediaApp

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SocialMediaApp());
+}
+
+class SocialMediaApp extends StatelessWidget {
+  const SocialMediaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Social Media App",
+    );
+  }
+}
