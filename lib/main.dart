2c87d73b0739d2767675c127b24d2b2c53e9dc06
hafizflow <hafizur.rahman.cs@gmail.com>
2024-10-16 13:52:05 +0600

    Firebase connected successfully

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class SocialMediaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Social Media App",
     );
   }
